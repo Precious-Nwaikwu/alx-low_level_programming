cac4f4759eb1fc1c9e92373471beff4145c74505
Precious.Nwaikwu <preciousfrancis3@gmail.com>
2022-04-12 15:55:45 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * 
+ *Printing alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
