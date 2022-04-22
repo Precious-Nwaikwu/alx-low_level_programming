556423f5f786ab5ede44502d03cf3b6b1b2a16d1
Precious.Nwaikwu <preciousfrancis3@gmail.com>
2022-04-21 20:29:03 -0700

    main.h file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,3 @@
-
 void reset_to_98(int *n); 
 void swap_int(int *a, int *b); 
 int _strlen(char *s); 
@@ -9,4 +8,5 @@ void puts2(char *str);
 void puts_half(char *str); 
 void print_array(int *a, int n); 
 char *_strcpy(char *dest, char *src); 
-intm _atoi(char *s);
+int _atoi(char *s);
+int _putchar(char c);
