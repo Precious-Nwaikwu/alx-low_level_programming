a9296a6b30ee0a8d43b2fea3fdcb3ef8b9bcaead
Precious.Nwaikwu <preciousfrancis3@gmail.com>
2022-04-21 20:08:48 -0700

    pointers, arrays2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,24 @@
-#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * _puts - prints a string, followed by a new line, 
- ​ * @str: pointer to the string to print 
- ​ * Return: void 
- ​*/ 
+#includes "main.h"
+
+
+/** 
+* _puts - prints a string, followed by a new line, 
+  * @str: pointer to the string to print 
+  * Return: void 
+ 
+ */ 
   
   
- ​void​ ​_puts​(​char​ *str) 
- ​{ 
- ​        ​int​ i = ​0​; 
+ void _puts(char *str) 
   
- ​        ​while​ (str[i]) 
- ​        { 
- ​                ​_putchar​(str[i]); 
- ​                i++; 
- ​        } 
+         int i = 0; 
   
- ​        ​_putchar​(​'​\n​'​); 
- ​} 
+         while (str[i]) 
+         { 
+                 _putchar(str[i]); 
+                 i++; 
+         } 
+  
+        _putchar('\n'); 
+ }
+
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,24 @@
-#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * print_rev - prints reversed string, followed by a new line 
- ​ * @s: pointer to the string to print 
- ​ * Return: void 
- ​*/ 
+#include "main.h"
+
+
+/** 
+ * print_rev - prints reversed string, followed by a new line 
+ * @s: pointer to the string to print 
+ * Return: void 
+ */ 
   
- ​void​ ​print_rev​(​char​ *s) 
- ​{ 
- ​        ​int​ i = ​0​; 
+ void print_rev(char *s) 
+ { 
+         int i = 0; 
   
- ​        ​while​ (s[i]) 
- ​                i++; 
+         while (s[i]) 
+                 i++; 
   
- ​        ​while​ (i--) 
- ​        { 
- ​                ​_putchar​(s[i]); 
- ​        } 
+         while (i--) 
+         { 
+                 _putchar(s[i]); 
+         } 
   
- ​        ​_putchar​(​'​\n​'​); 
+         _putchar('\n'); 
   
- ​}
+}
