96aa4e2ce653a173efc69dd15c69f32b21426419
Precious.Nwaikwu <preciousfrancis3@gmail.com>
2022-04-24 05:20:41 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
-#include "main.h"
 
+#include "main.h"
 /**
- * _strncat - concatenates two strings,
+ * _strncat - concatenates two strings
  * @dest: destination.
  * @src: source.
  * @n: amount of bytes used from src.
