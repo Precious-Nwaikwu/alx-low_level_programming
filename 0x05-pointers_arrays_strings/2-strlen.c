c73020ca5c7ce9b8cc2f4c2cfe5abeb81f7b960c
Precious.Nwaikwu <preciousfrancis3@gmail.com>
2022-04-25 12:26:29 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 
 #include "main.h"
 
-
 /**
- * _strlen - find the lenght of a string
+ * _strlen - find the length of a string
  * @s: pointer to the string to check
  * Return: void
  */
+
 
 int _strlen(char *s)
 {
