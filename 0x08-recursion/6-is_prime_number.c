2931fe3c859c364b8aced088dbb0585b9cc836ad
Precious.Nwaikwu <preciousfrancis3@gmail.com>
2022-05-01 11:40:11 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,7 +7,7 @@
  * @c: iterator.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-int is_prime_number(unsigned int n, unsigned int c)
+int is_prime(unsigned int n, unsigned int c)
 {
 	if (n % c == 0)
 	{
