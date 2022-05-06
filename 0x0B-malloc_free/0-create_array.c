3d109292f560c7acd28a3e6363a72cd0f8087d15
Precious.Nwaikwu <preciousfrancis3@gmail.com>
2022-05-05 21:44:55 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,10 +18,10 @@ char *create_array(unsigned int size, char c)
 
 	cr = malloc(sizeof(c) * size);
 
-	if(cr == NULL)
+	if (cr == NULL)
 		return (NULL);
 
-	for (i = 0; i < size, i++)
+	for (i = 0; i < size; i++)
 		cr[i] = c;
 
 	return (cr);
