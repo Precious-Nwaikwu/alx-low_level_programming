3e4e0bccacff05bfb1ef0dafadc6c782e1dab1cd
Precious.Nwaikwu <preciousfrancis3@gmail.com>
2022-05-10 15:48:42 -0700

    header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,11 +12,10 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 
-/*functions for 101-mul.c */
+/* functions for 101-mul.c */
 
 /**
- * error_exit - prints error with _putchar
- * 		and exits with 98
+ * error_exit - prints error with _putchar and exits with 98
  *
  * Return: Error 98 and exit(98)
  */
@@ -34,8 +33,7 @@ int error_exit(void)
 }
 
 /**
- * check_number - checks if string has only
- *		numbers
+ * check_number - checks if string has only numbers
  *
  * @str: string to check
  *
