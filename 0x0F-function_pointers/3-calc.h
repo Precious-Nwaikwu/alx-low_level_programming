bc3ed4d7117f4cacc2df039f8e10503ab92bb795
Precious.Nwaikwu <preciousfrancis3@gmail.com>
2022-05-15 16:18:22 -0700

    header file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -20,11 +20,11 @@ int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
-int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif
+
 
 function_pointers.h
 
@@ -38,4 +38,3 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
-
